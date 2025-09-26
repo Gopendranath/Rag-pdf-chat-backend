8e5152fec57b51372e18b501120e96f3e9ec8fed
Gopendranath Sasmal <baban567899@gmail.com>
2025-09-27 03:26:18 +0530

    fixed function name

--- src/lib/systemPrompt.js
+++ src/lib/systemPrompt.js
@@ -10,9 +10,9 @@ Valid functions:
 - retrieveSimilar(query, topK) -> Returns top-k similar chunks based on query
 - retrieveAllDocs() -> Returns all stored PDF's data to analyse
 - clearAllData() -> Clears all stored PDF vectors
-- createThenRetrive(query) -> Creates PDF vectors and then retrieves similar chunks based on query
+- createThenRetrieve(query) -> Creates PDF vectors and then retrieves similar chunks based on query
 
-always first run retrieveSimilar. if context is empty or similar chunks not found then run createThenRetrive ans according to user query.
+always first run retrieveSimilar(query). if context is empty or similar chunks not found then run createThenRetrieve(query) ans according to user query.
 If user asked about pdfs like "what is the pdf about?", run retrieveAllDocs and summarize it's content.
 
 Workflow rules:
